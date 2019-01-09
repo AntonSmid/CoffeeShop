3518f83bb12ca136b1ad355c3503549a3fa5a437
AntonSmid <anton.smid@triera.net>
2019-01-09 19:37:47 +0100

    added JS for mail form

--- js/app.js
+++ js/app.js
@@ -23,6 +23,8 @@ document.querySelector(".video__switch").addEventListener("click", function(){
   ui.videoControls();
 })
   
+  
+  
 // submit the form 
   document.querySelector(".drink-form").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -34,7 +36,14 @@ document.querySelector(".video__switch").addEventListener("click", function(){
     // console.log(value);
     
     if(value) {
+      let customer = new Customer(name, lastName, email)
+      console.log(customer);
+      
+      ui.addCustomer(customer)
+  
       ui.showFeedback("customer added to the list", "success")
+      
+      ui.clearFields()
     } 
       else 
     {
@@ -90,14 +99,13 @@ UI.prototype.checkEmpty = function(name, lastname, email) {
 
 
 UI.prototype.showFeedback = function (text, type) {
-  if (type === "success"){
-    let feedback = document.querySelector(".drink-form__feedback");
+  const feedback = document.querySelector(".drink-form__feedback");
+  if (type === "success"){  
     feedback.classList.add("success");
     feedback.innerText = text;
     this.removeAlert("success");   
   } 
   else if (type === "error") {
-    let feedback = document.querySelector(".drink-form__feedback");
     feedback.classList.add("error");
     feedback.innerText = text;
     this.removeAlert("error");
@@ -110,6 +118,41 @@ UI.prototype.removeAlert = function (type) {
   setTimeout(function(){
     document.querySelector(".drink-form__feedback").classList.remove(type)
   }, 3000)
+}
+
+
+
+// add customer
+UI.prototype.addCustomer = function(customer) {
+  const images = [1, 2, 3, 4, 5];
+  let random = Math.floor(Math.random()*images.length);
+  const div = document.createElement("div");
+  
+  div.classList.add("person");
+  
+  div.innerHTML = `<img src="img/person-${random}.jpeg" alt="person"
+class="person__thumbnail">
+  <h4 class="person__name">${customer.name}</h4>
+<h4 class="person__last-name">${customer.lastname}</h4>`
+  
+  document.querySelector(".drink-card__list").appendChild(div)
+}
+
+// clear fields
+UI.prototype.clearFields = function() {
+  document.querySelector(".input-name").value = "";
+  document.querySelector(".input-lastname").value = "";
+  document.querySelector(".input-email").value = "";
+}
+
+
+
+
+
+function Customer (name, lastname, email) {
+  this.name = name,
+  this.lastname = lastname,
+  this.email = email;
 }
 
 
